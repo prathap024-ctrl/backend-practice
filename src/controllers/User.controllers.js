009ca60ed7eaead7a7938444fe713a3faa9378cf
prathap024-ctrl <prathaplokesh04@gmail.com>
2025-03-27 13:11:05 +0530

    modified

--- src/controllers/User.controllers.js
+++ src/controllers/User.controllers.js
@@ -421,6 +421,10 @@ const getWatchHistory = asyncHandler(async (req, res) => {
       },
     },
   ]);
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user[0].watchHistory, "Watch history fetched!"));
 });
 
 export {
