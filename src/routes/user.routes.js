3d8f2205caa5bc84a1f90ea6075e5cb454485df1
prathap024-ctrl <prathaplokesh04@gmail.com>
2025-03-27 13:33:39 +0530

    modified

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -46,11 +46,11 @@ router.route("/update-account").post(verifyJWT, updateAccountDetails);
 
 router
   .route("/avatar")
-  .patch(verifyJWT, upload.single("/avatar"), updateUserAvatar);
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
 
 router
   .route("/cover-image")
-  .patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage);
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
 
 router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
 
